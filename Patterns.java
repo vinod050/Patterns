8ec14fc198c4321c7139777de1ea5ccd8d540752
vinod kumar <sirikondaviinod99@gmail.com>
2025-01-29 16:58:51 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,21 @@
+public class Patterns {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+        
+            for (int j = 1; j <= 5; j++){
+                if (i==2 || j==2||j==4) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            //     System.out.print("*");
+       
+            System.out.print(" ");
+            }
+            System.out.println(" ");
+            // System.out.println("*");
+            // System.out.println("*");
+        }
+    }
+    
+}
